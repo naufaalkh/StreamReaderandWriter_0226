4868591880d618f9b9190e6bc7523beb33144312
naufaalkh <naufaalkh@gmail.com>
2024-06-04 15:13:55 +0700

    menangkap menggunakan objek expection

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -13,4 +13,10 @@ int main()
         cout<<data.at(5)<<endl;
         // memanggil array elemen ke 5
     }
+    catch (exception &e)
+    {
+        // penangkan menggunakan objek expection
+        cout << e.what() << endl;
+        // akan dieksekusi karna array data hanya memiliki 3 elemen
+    }
 }
