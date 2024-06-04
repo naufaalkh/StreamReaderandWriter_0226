7636fc4c9011ec3059661d36b208849bdf7adfcc
naufaalkh <naufaalkh@gmail.com>
2024-06-04 15:04:28 +0700

    menambahkan untuk membuat custom nama file

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File :";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan q
+        if (baris == "q")
+            break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup filenya
+}
