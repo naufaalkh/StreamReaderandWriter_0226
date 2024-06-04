84d4cc2dcd94c8a87c026683727607115678da1b
naufaalkh <naufaalkh@gmail.com>
2024-06-04 14:43:02 +0700

    membuat output untuk keterangan

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,4 +11,8 @@ int main() {
     ofstream outfile;
     // menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    
 }
