725b33c4d82d19b7b84232382cf10d65757b75d5
naufaalkh <naufaalkh@gmail.com>
2024-06-04 15:13:29 +0700

    membuat awal prgram, pesan array int 3 elemen dan memanggil array elemen ke 5

---
+++ exceptionStandard.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+// untuk objek exception yang akan digunakan
+#include <array>
+//  untuk objek array yang akan kita gunakan
+using namespace std;
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:..
+    try{
+        array<int, 3> data = { 3, 5, 7 };
+        // pesan array interger 3 elemen
+        cout<<data.at(5)<<endl;
+        // memanggil array elemen ke 5
+    }
+}
