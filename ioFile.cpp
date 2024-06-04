0d08b82c5e9d0a0ac031a080320ab6c976dd8e65
naufaalkh <naufaalkh@gmail.com>
2024-06-04 15:03:25 +0700

    jika ada file membuat perulangan baris,  menampilkan dan menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -35,4 +35,16 @@ int main() {
     infile.open("contohfile.txt");
 
     cout << endl<< ">= membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan disini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
 }
